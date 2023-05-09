07c5e39621990264a3882ccad887be0ed054ca53
ria1809 <riarox667@gmail.com>
2023-05-09 13:07:44 +0200

    first commit

---
+++ main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include "gridMap.h"
+#include "threepp/threepp.hpp"
+using namespace threepp;
+
+
+int main() {
+    int i,j;
+    gridMap grid; // printing out after this line will print out the grid with values assigned 0
+    grid.initializeGrid();// function for assigning 0 and 1 randomly
+    grid.display();// printing out after this line will print out the grid with random assignments of 0 and 1
+    std::cout << "Enter row and column (0-4) to toggle the light: ";
+
+
+    while(cin>> i>>j) {
+        grid.toggle(i, j);
+        grid.display();
+        grid.win();
+    }
+
+   return 0;
+}
